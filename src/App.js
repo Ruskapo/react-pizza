a5461205f245bc8f5b6b72a6ffbc6f003b673343
Alexandr Lebedev <lebedev-2018@bk.ru>
2025-12-06 22:43:16 +0500

    fetch & useeffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react'
 import Categories from "./components/Categories";
 import Header from "./components/Header";
 import PizzaBlock from "./components/PizzaBlock";
 import Sort from "./components/Sort";
-import pizzas from './assets/pizzas.json'
 import "./scss/app.scss";
 
 function App() {
+  const [itemsPizza, setItemsPizza] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://69344d6a4090fe3bf01f91ec.mockapi.io/items')
+        .then((Response) => Response.json())
+        .then((obj) => {
+          setItemsPizza(obj);
+        });
+  }, []);
+
+
+
+
   return (
     <div className="wrapper">
       <Header />
@@ -18,7 +31,7 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              pizzas.map((objPizz) => (
+              itemsPizza.map((objPizz) => (
                 <PizzaBlock
                  key={objPizz.id}
                  title = {objPizz.title}
