bda2ddffc36ba1598099af7f38072fd5b3add60b
Alexandr Lebedev <cockato0.lae@gmail.com>
2025-12-16 00:48:13 +0500

    feat: add pagination and search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import Search from "./Search";
 
-function Header() {
+function Header({ searchValue, setSearchValue }) {
   return (
     <div className="header">
       <div className="container">
@@ -15,7 +15,7 @@ function Header() {
           </div>
         </Link>
 
-        <Search/>
+        <Search searchValue={searchValue} setSearchValue={setSearchValue}  />
 
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
+
 import Categories from "../components/Categories";
 import PizzaBlock from "../components/PizzaBlock/";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import Sort from "../components/Sort";
+import Pagination from "../components/Pagination";
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [itemsPizza, setItemsPizza] = React.useState([]);
   const [isLoading, setLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSortType] = React.useState({
     name: "популярности",
     sortProperty: "rating",
@@ -19,17 +22,37 @@ const Home = () => {
     const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
     const sortBy = sortType.sortProperty.replace("-", "");
     const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const search = searchValue ? `&search=${searchValue}` : "";
+
     fetch(
-      `https://69344d6a4090fe3bf01f91ec.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+      `https://69344d6a4090fe3bf01f91ec.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((Response) => Response.json())
-      .then((arr) => {
-        setItemsPizza(arr);
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setItemsPizza(data);
+        } else setItemsPizza([]);
+
         setLoading(false);
       });
 
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = itemsPizza.map((objPizz) => (
+    <PizzaBlock
+      key={objPizz.id}
+      title={objPizz.title}
+      price={objPizz.price}
+      image={objPizz.imageUrl}
+      sizes={objPizz.sizes}
+      types={objPizz.types}
+    />
+  ));
+
+  const sceletons = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
 
   return (
     <div className="container">
@@ -41,20 +64,8 @@ const Home = () => {
         <Sort value={sortType} onClickSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : itemsPizza.map((objPizz) => (
-              <PizzaBlock
-                key={objPizz.id}
-                title={objPizz.title}
-                price={objPizz.price}
-                image={objPizz.imageUrl}
-                sizes={objPizz.sizes}
-                types={objPizz.types}
-              />
-            ))}
-      </div>
+      <div className="content__items">{isLoading ? sceletons : pizzas}</div>
+      <Pagination onChangePage={number => setCurrentPage(number)}  />
     </div>
   );
 };
